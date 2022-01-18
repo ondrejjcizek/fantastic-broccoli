380894c03d294283f171c52b4b2b9366141f32b1
Ondřej Čížek <ondrejcizek@Ondrejs-MacBook-Pro.local>
2022-01-18 21:42:02 +0100

    Adding Basic CSS Styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className='expense-item'>
             <div>January 18th 2021</div>
-            <div>
+            <div className='expense-item__description'>
                 <h2>Car Insurance</h2>
-                <div>$294.67</div>
+                <div className='expense-item__price'>$294.67</div>
             </div>
         </div>
-    )
+    );
 }
 
 export default ExpenseItem;
