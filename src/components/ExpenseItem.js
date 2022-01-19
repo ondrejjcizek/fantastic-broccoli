e629096325c81674675c2a765147883ec35c7e55
Ondřej Čížek <ondrejcizek@Ondrejs-MacBook-Pro.local>
2022-01-19 18:11:23 +0100

    Outputting Dynamic Data & Working with Expressions in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 1, 19);
+    const expenseTitle = 'Card Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className='expense-item'>
-            <div>January 18th 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div>
     );
