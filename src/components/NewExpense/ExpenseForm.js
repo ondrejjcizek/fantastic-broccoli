2936baa5ad54f7fd712fe93ebce3285c34580d90
Ondřej Čížek <ondrejcizek@Ondrejs-MacBook-Pro.local>
2022-01-22 11:49:42 +0100

    Child-to-Parent Component Communication (Bottom-up)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,54 +1,21 @@
 import { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState();
     const [enteredAmount, setEnteredAmount] = useState();
     const [enteredDate, setEnteredDate] = useState();
 
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: ''
-    // });
-
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
-
-        // setUserInput({
-        //     ...userInput,
-        //     enteredTitle: event.target.value
-        // });
-        
-        // setUserInput((prevState) => {
-        //     return { ...prevState, enteredTitle: event.target.value }
-        // });
     };
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
-
-        // setUserInput({
-        //     ...userInput,
-        //     enteredDate: event.target.value
-        // });
-
-        // setUserInput((prevState) => {
-        //     return { ...prevState, enteredAmount: event.target.value }
-        // });
     };
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
-
-        // setUserInput({
-        //     ...userInput,
-        //     enteredAmount: event.target.value
-        // });
-
-        // setUserInput((prevState) => {
-        //     return { ...prevState, enteredDate: event.target.value }
-        // });
     };
 
     const submithHandler = (event) => {
@@ -60,7 +27,8 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         };
 
-        console.log(expenseData);
+        props.onSaveExpenseData(expenseData); // Passing data to NewExpense Component
+
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
