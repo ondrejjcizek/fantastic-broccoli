141458c03bffd4915dd6b17062b64afb61031a44
Ondřej Čížek <ondrejcizek@Ondrejs-MacBook-Pro.local>
2022-01-22 16:46:38 +0100

    Filtering Items by Year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,12 +11,17 @@ function Expenses(props) {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
-                {props.items.map((expense) => (
-                    <ExpenseItem 
+                {filteredExpenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
                         title={expense.title}
                         amount={expense.amount}
                         date={expense.date}
