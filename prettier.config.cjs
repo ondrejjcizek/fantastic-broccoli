3e694fc20d403389db15bfeb489ac202ef6fa814
Ondřej Čížek <ondrej.cizek@giant.cz>
2022-12-16 10:01:07 +0100

    Tailwind sorting class

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+{
+  module.exports = {
+    plugins: [require('prettier-plugin-svelte'), require('prettier-plugin-tailwindcss')],
+    tailwindConfig: './tailwind.config.cjs',
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: 'none',
+    printWidth: 100
+    // pluginSearchDirs: ["."],
+    // overrides: [{ "files": "*.svelte", "options": { "parser": "svelte" } }]
+  };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1
+  class="relative flex h-[80px] items-center justify-center bg-black text-center text-3xl font-bold text-white underline"
+>
+  Hello world!
+</h1>
