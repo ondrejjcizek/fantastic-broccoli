561bfa5a9127f98f5b970d8fdb9cb81b638ed50e
Ondřej Čížek <ondrej.cizek@giant.cz>
2023-01-22 19:22:40 +0100

    Animating the Search Input field

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
-  import Search from '$components/Icon/Search.svelte';
+    import Search from '$components/Icon/Search.svelte';
 </script>
 
 <div class="relative flex w-full items-baseline">
-  <div class="mr-2 mt-auto text-pastelPurple">
-    <Search />
-  </div>
-  <input type="text" name="search" placeholder="Hledej ve fakturách" class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl" />
-  <button class="font-sansSerif text-lg font-black text-pastelPurple lg:text-xl">Vyhledat</button>
+    <div class="mr-2 mt-auto text-pastelPurple">
+        <Search />
+    </div>
+    <input
+        type="text"
+        name="search"
+        placeholder="Hledej ve fakturách"
+        class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent pr-[90px] font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] md:pr-0 lg:w-72 lg:text-xl"
+    />
+    <button
+        class="pointer-events-none absolute left-8 transform font-sansSerif text-lg font-black text-pastelPurple transition-transform transition lg:text-xl"
+        >Vyhledat</button
+    >
 </div>
+
+<style lang="postcss">
+    ::placeholder {
+        color: transparent;
+    }
+
+    input:not(:placeholder-shown) + button,
+    input:focus + button {
+        @apply pointer-events-auto left-auto right-0 translate-x-[calc(100%-75px)] text-right text-lavenderIndigo md:right-auto md:left-0 md:translate-x-[16rem] lg:translate-x-80;
+    }
+
+    input + button:hover,
+    input:focus + button:hover {
+        @apply text-daisyBush;
+    }
+</style>
