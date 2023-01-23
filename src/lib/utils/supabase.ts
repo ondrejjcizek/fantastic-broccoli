172f2cf81b6a8522a9963ac44d297de8ed258c7f
Ondřej Čížek <ondrej.cizek@giant.cz>
2023-01-23 17:54:45 +0100

    fix deploy

--- src/lib/utils/supabase.ts
+++ src/lib/utils/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '$env/dynamic/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const supabaseUrl = env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = env.PUBLIC_SUPABASE_ANON_KEY;
+console.log(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+
+const supabaseUrl = PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
 
