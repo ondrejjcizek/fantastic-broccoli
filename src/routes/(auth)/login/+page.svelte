54df51313ea58efaeec4e0a732c8d2bd37f5dc56
Ondřej Čížek <ondrej.cizek@giant.cz>
2023-01-30 21:51:33 +0100

    Login

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,33 +1,61 @@
 <script lang="ts">
     import Alert from '$components/Alert.svelte';
+    import Loader from '$components/Loader.svelte';
+    import supabase from '$utils/supabase';
+    import { goto } from '$app/navigation';
+
     let email: string,
         password: string,
-        alertMessage = 'Byli jste úspěšně odhlášeni!';
+        message = 'Byli jste úspěšně odhlášeni!',
+        isLoading = false;
+
+    const handleSubmit = async () => {
+        isLoading = true;
+        message = '';
+        let { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        });
+
+        if (error) {
+            message = error.message;
+            console.log(error);
+        } else {
+            goto('/invoices');
+        }
+        isLoading = false;
+    };
 </script>
 
 <h1 class="auth-heading">Přihlášení</h1>
 
-<form>
-    <Alert message={alertMessage} />
-    <div class="field">
-        <label for="email" class="text-goldenFizz">E-mail</label>
-        <input type="email" name="email" placeholder="Váš E-mail" bind:value={email} />
-    </div>
-
-    <div class="field">
-        <div class="flex justify-between">
-            <label for="password" class="text-goldenFizz">Heslo</label>
-            <a href="/forgot-password" class="text-sm text-white underline hover:no-underline"
-                >Zapomenuté heslo?</a
-            >
+<form on:submit|preventDefault={handleSubmit}>
+    <Alert {message} />
+    <fieldset disabled={isLoading}>
+        <div class="field">
+            <label for="email" class="text-goldenFizz">E-mail</label>
+            <input type="email" name="email" placeholder="Váš E-mail" bind:value={email} />
         </div>
-        <input type="password" placeholder="Vaše heslo" bind:value={password} />
-    </div>
-
-    <div class="field">
-        <button type="submit" class="auth-button"> Pojďme na to! </button>
-        <p class="mt-4 text-center text-sm text-white">
-            <a href="/signup" class="underline hover:no-underline">Ještě nemáte účet?</a>
-        </p>
-    </div>
+        <div class="field">
+            <div class="flex justify-between">
+                <label for="password" class="text-goldenFizz">Heslo</label>
+                <a href="/forgot-password" class="text-sm text-white underline hover:no-underline"
+                    >Zapomenuté heslo?</a
+                >
+            </div>
+            <input type="password" placeholder="Vaše heslo" bind:value={password} />
+        </div>
+        <div class="field">
+            <button type="submit" class="auth-button flex items-center justify-center gap-x-2">
+                {#if isLoading}
+                    <Loader /> Načítám
+                {:else}
+                    Pojďme na to!
+                {/if}
+            </button>
+            <p class="mt-4 text-center text-sm text-white">
+                <a href="/signup" class="underline hover:no-underline">Ještě nemáte účet?</a>
+            </p>
+        </div>
+    </fieldset>
 </form>
--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,25 +1,102 @@
-<h1 class="auth-heading">Sign Up</h1>
-<form>
-    <div class="field">
-        <label for="fullName" class="text-goldenFizz">Celé jméno</label>
-        <input type="text" name="fullName" id="fullName" required />
-    </div>
-    <div class="field">
-        <label for="email" class="text-goldenFizz">E-mail</label>
-        <input type="email" name="email" id="email" required />
-    </div>
+<script lang="ts">
+    import supabase from '$utils/supabase';
+    import Alert from '$components/Alert.svelte';
+    import Loader from '$components/Loader.svelte';
 
-    <div class="field">
-        <label for="password" class="text-goldenFizz">Heslo</label>
-        <input type="password" name="password" id="password" required />
-    </div>
+    let fullName: string,
+        email: string,
+        password: string,
+        confirmPassword: string,
+        message: string = '',
+        isSignUpFormShowing: boolean = true,
+        isLoading: boolean = false;
 
-    <div class="field">
-        <label for="confirmPassword" class="text-goldenFizz">Zopakujte heslo</label>
-        <input type="password" name="confirmPassword" id="confirmPassword" required />
-    </div>
-    <button type="submit" class="auth-button">Registrovat</button>
-    <p class="mt-4 text-center text-sm text-white">
-        <a href="/login" class="underline hover:no-underline">Máte již vytvořený účet?</a>
-    </p>
-</form>
+    const handleSubmit = async () => {
+        isLoading = true;
+        message = '';
+
+        // make sure that the password and the confirm passwrod match
+        if (password !== confirmPassword) {
+            message = "Your password and confirm password don't match";
+            isLoading = false;
+            return;
+        }
+
+        let userResults = await supabase.auth.signUp({ email, password });
+
+        if (userResults.error) {
+            message = userResults.error.message;
+            console.error(userResults.error);
+            isLoading = false;
+            return;
+        }
+
+        // add the full name to the settings table
+        if (userResults?.data?.user?.id) {
+            const settingsResults = await supabase
+                .from('settings')
+                .insert([{ myName: fullName, userId: userResults.data.user.id }]);
+
+            if (settingsResults.error) {
+                message = settingsResults.error.message;
+                console.error(settingsResults.error);
+                isLoading = false;
+                return;
+            }
+        }
+
+        isSignUpFormShowing = false;
+        isLoading = false;
+    };
+</script>
+
+{#if isSignUpFormShowing}
+    <h1 class="auth-heading">Sign Up</h1>
+    <Alert {message} />
+    <form on:submit|preventDefault={handleSubmit}>
+        <fieldset disabled={isLoading}>
+            <div class="field">
+                <label for="fullName" class="text-goldenFizz">Celé jméno</label>
+                <input type="text" name="fullName" id="fullName" required bind:value={fullName} />
+            </div>
+            <div class="field">
+                <label for="email" class="text-goldenFizz">E-mail</label>
+                <input type="email" name="email" id="email" required bind:value={email} />
+            </div>
+            <div class="field">
+                <label for="password" class="text-goldenFizz">Heslo</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    required
+                    bind:value={password}
+                />
+            </div>
+            <div class="field">
+                <label for="confirmPassword" class="text-goldenFizz">Zopakujte heslo</label>
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    required
+                    bind:value={confirmPassword}
+                />
+            </div>
+            <button type="submit" class="auth-button flex items-center justify-center gap-x-2">
+                {#if isLoading}
+                    <Loader />
+                    Načítám
+                {:else}
+                    Registrovat
+                {/if}
+            </button>
+            <p class="mt-4 text-center text-sm text-white">
+                <a href="/login" class="underline hover:no-underline">Máte již vytvořený účet?</a>
+            </p>
+        </fieldset>
+    </form>
+{:else}
+    <Alert message="Do emailu vám dorazí potvrzovací odkaz" />
+    <a href="/login" class="auth-button block text-center">Přihlásit se</a>
+{/if}
